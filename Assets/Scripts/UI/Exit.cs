34eda7aa55518e883571cf61ba32f6fee398ca75
Llwnr <noregrets.llwnr@gmail.com>
2023-12-11 08:37:46 +0545

    .

--- Assets/Scripts/UI/Exit.cs
+++ Assets/Scripts/UI/Exit.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Exit : MonoBehaviour
 {
     public void ExitGame() {
+        #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
+        #endif
         Application.Quit();
     }
 }
